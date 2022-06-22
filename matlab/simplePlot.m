3b44810f6a937c2b184134139572c803644709e2
think92 <sikaoguo@gmail.com>
2022-06-22 17:43:35 -0400

    simplePlot modified

--- matlab/simplePlot.m
+++ matlab/simplePlot.m
@@ -2,6 +2,8 @@
 clear;
 clc;
 
-x = 1:3
-y = sqrt(x)
-
+x = 0 : 0.1 : 10;
+y = sin(x);
+plot(x,y,'-rs','LineWidth',2,'MarkerEdgeColor','k','MarkerSize',5),grid
+xlabel('x'),ylabel('sin(x)')
+whitebg('y')
